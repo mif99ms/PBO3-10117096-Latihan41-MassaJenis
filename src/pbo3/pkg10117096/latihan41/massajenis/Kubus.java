ece04cd0e6b600c9796a36c3ef2c0d030b5596a5
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-10-22 19:15:46 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan41/massajenis/Kubus.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan41.massajenis;
+
+/**
+ *
+ * @author 
+ * Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO3
+ */
+public class Kubus {
+     private int sisi,massa;
+
+    public int getSisi() {
+        return sisi;
+    }
+
+    public void setSisi(int sisi) {
+        this.sisi = sisi;
+    }
+
+    public int getMassa() {
+        return massa;
+    }
+
+    public void setMassa(int massa) {
+        this.massa = massa;
+    }
+    
+    public int hitungVolume(int parSisi){
+        return sisi*sisi*sisi;
+        
+    }
+    
+    public int hitungMassaJenis(int parMassa, int volume){
+        return massa / (sisi * sisi * sisi);
+        
+    }
+}
---
+++ src/pbo3/pkg10117096/latihan41/massajenis/PBO310117096Latihan41MassaJenis.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan41.massajenis;
+
+/**
+ *
+ * @author 
+ * Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO3
+ * Deskripsi    : Program ini berisi program untuk menghitung massa jenis
+ */
+public class PBO310117096Latihan41MassaJenis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Kubus objK = new Kubus();
+        System.out.println("==== Massa Jenis Kubus====");
+        objK.setSisi(5);
+        objK.setMassa(250);
+        System.out.println("Sisi : "+objK.getSisi());
+        System.out.println("Massa : "+objK.getMassa());
+        System.out.println("");
+        System.out.println("==== Hasil Perhitungan====");
+        System.out.println("Volume : "+objK.hitungVolume(0));
+        System.out.println("Massa Jenis : "+objK.hitungMassaJenis(0,0));
+
+    }
+    
+}
